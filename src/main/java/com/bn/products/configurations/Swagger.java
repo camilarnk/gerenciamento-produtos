b6d88b736042c85f68371c9d650e84146dd5160c
Camila Reinke <reinkecamila@gmail.com>
2026-03-18 14:39:35 -0300

    chore: adiciona arquivos de configuração do projeto

---
+++ src/main/java/com/bn/products/configurations/Swagger.java
@@ -0,0 +1,8 @@
+package com.bn.products.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+// localhost:8080/swagger-ui.html
+@Configuration
+public class Swagger {
+}
